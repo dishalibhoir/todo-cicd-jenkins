5e190b0ac192be17caec76a93c566c56e7c0fd83
dishalibhoir <155667918+dishalibhoir@users.noreply.github.com>
2025-04-20 13:45:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,47 +1,42 @@
-// Requiring the assert module for assertions
+// Requiring module
 const assert = require('assert');
 
-// Grouping similar tests inside a describe block
+// We can group similar tests inside a describe block
 describe("Simple Calculations", () => {
-
     before(() => {
-        // This part executes once before all tests
         console.log("This part executes once before all tests");
     });
 
     after(() => {
-        // This part executes once after all tests
         console.log("This part executes once after all tests");
     });
-    
-    // Nested blocks for different sets of tests
+
+    // We can add nested blocks for different tests
     describe("Test1", () => {
         beforeEach(() => {
-            // This part executes before every test in this block
-            console.log("executes before every test in Test1");
-        });
-        
-        it("should return 5 when adding 2 + 3", () => {
-            assert.equal(2 + 3, 5); // Check if 2 + 3 equals 5
+            console.log("executes before every test");
         });
 
-        it("should return 6 when multiplying 2 * 3", () => {
-            assert.equal(2 * 3, 6); // Check if 2 * 3 equals 6
+        it("Is returning 5 when adding 2 + 3", () => {
+            assert.equal(2 + 3, 5);
+        });
+
+        it("Is returning 6 when multiplying 2 * 3", () => {
+            assert.equal(2 * 3, 6);
         });
     });
 
     describe("Test2", () => {
         beforeEach(() => {
-            // This part executes before every test in this block
-            console.log("executes before every test in Test2");
+            console.log("executes before every test");
         });
 
-        it("should return 5 when adding 2 + 3", () => {
-            assert.equal(2 + 3, 5); // Check if 2 + 3 equals 5
+        it("Is returning 5 when adding 2 + 3", () => {
+            assert.equal(2 + 3, 5);
         });
 
-        it("should return 8 when multiplying 2 * 4", () => {
-            assert.equal(2 * 4, 8); // Check if 2 * 4 equals 8
+        it("Is returning 8 when multiplying 2 * 4", () => {
+            assert.equal(2 * 4, 8);
         });
     });
 });
